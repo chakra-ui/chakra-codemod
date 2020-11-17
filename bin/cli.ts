6659dbf00a1e724c0a9203c0c9dc156a4204c725
Tim Kolberger <tim@kolberger.eu>
2020-11-17 19:23:22 +0100

    chore: check for clean workdir and ask which codemods to run

--- bin/cli.ts
+++ bin/cli.ts
@@ -3,6 +3,8 @@
 import fs from "fs";
 import chalk from "chalk";
 import path from "path";
+import isGitClean from "is-git-clean";
+import inquirer from "inquirer";
 
 function getWorkingDir() {
   const workingDir = path.resolve(process.argv[2] || process.cwd());
@@ -17,9 +19,65 @@ function getWorkingDir() {
   return workingDir;
 }
 
+async function isWorkdirClean(workingDir: string) {
+  try {
+    return await isGitClean(workingDir);
+  } catch {
+    // workdir is not a git repo
+    return true;
+  }
+}
+
+async function askQuestions(options: { codemods: string[] }) {
+  const allText = "ALL";
+  const answers: {
+    mods: string[];
+    isDryRun: boolean;
+  } = await inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "mods",
+      message: "Which code mods do you want to run?",
+      choices: [allText, ...options.codemods],
+      default: [allText],
+    },
+    {
+      type: "confirm",
+      name: "isDryRun",
+      message: "Do you want to perform a dry run?",
+      default: true,
+    },
+  ]);
+
+  if (answers.mods.includes(allText)) {
+    // if "ALL" are selected, return every available codemod
+    return {
+      ...answers,
+      mods: options.codemods.slice(),
+    };
+  }
+
+  return answers;
+}
+
 export async function bootstrap() {
   const workingDir = getWorkingDir();
   console.log(`${chalk.blue`Run chakra codemod in: `}${chalk(workingDir)}`);
+
+  const isWorkingDirClean = await isWorkdirClean(workingDir);
+  if (!isWorkingDirClean) {
+    console.log(
+      chalk.yellow`There are uncommited changes in your working directory.
+Please commit or stash them before running the codemode.`,
+    );
+  }
+
+  // TODO get real codemod names
+  const codemods = ["these", "are", "the", "names", "of", "our", "codemods"];
+  const answers = await askQuestions({ codemods });
+
+  // TODO run it 💨
+  console.log(answers);
 }
 
 bootstrap().catch((e) => {
