4944a723ebdd35dd458640f76bedf7079416f5cb
Martian2Lee <martian2lee@163.com>
2020-11-18 16:44:57 +0400

    add test for update import

---
+++ transforms/__tests__/update-import.ts
@@ -0,0 +1,20 @@
+/* global jest */
+
+jest.autoMockOff();
+
+import { defineTest } from "jscodeshift/dist/testUtils";
+
+/**
+ * List of all test fixtures with an input and
+ * output pair for each.
+ */
+const fixtures = ["basic"] as const;
+const name = "update-import";
+
+describe(name, () => {
+  fixtures.forEach((test) =>
+    defineTest(__dirname, name, null, `${name}/${test}`, {
+      parser: "tsx",
+    }),
+  );
+});
