cc99e87affc06c23813b21e2b3b29980ccc539a2
Martian2Lee <martian2lee@163.com>
2020-11-18 16:44:42 +0400

    add comment

--- transforms/__tests__/use-disclosure-api.ts
+++ transforms/__tests__/use-disclosure-api.ts
@@ -4,6 +4,10 @@ jest.autoMockOff();
 
 import { defineTest } from "jscodeshift/dist/testUtils";
 
+/**
+ * List of all test fixtures with an input and
+ * output pair for each.
+ */
 const fixtures = ["basic"] as const;
 const name = "use-disclosure-api";
 
